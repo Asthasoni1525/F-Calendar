dac68c7a73b3b596d448effcf732bf6f7662dc34
Asthasoni1525 <asthasoni2508@gmail.com>
2023-11-27 18:48:24 +0530

    Appointment Added

--- js/script.js
+++ js/script.js
@@ -39,7 +39,14 @@
             start: "2023-11-27T04:00:00",
             end: "2023-11-27T06:00:00",
             resourceId: 'c'
-        }
+        },
+        {
+          id: 'd',
+          title: 'Astha',
+          start: "2023-11-27T04:00:00",
+          end: "2023-11-27T06:00:00",
+          resourceId: 'c'
+      }
       ];
 
     var calendar = new FullCalendar.Calendar(calendarEl, {
